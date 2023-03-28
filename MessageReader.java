02ef11804ce2ee4a26ecb1325e79521faaa92a5d
@VIKASGUPTA <108696132+VGJSK-source@users.noreply.github.com>
2023-03-28 22:57:49 +0530

    Salary_Calculation_Application
    I have made a salary calculation java based application. By using this any employee can calculate their salary by just taking the base salary.

---
+++ MessageReader.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MessageReader {
+    ResourceBundle resourceBundle;
+    MessageReader(){
+        Locale locale= new Locale("en", "US");
+        resourceBundle= ResourceBundle.getBundle("message",locale);
+    }
+
+    public String getMessage(String key) {
+        return resourceBundle.getString(key);
+    }
+
+    }
+
